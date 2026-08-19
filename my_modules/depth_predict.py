3297f6a152321762d10bea36f77081a53f9a2802
JYC <jiangyichen.x@qq.com>
2026-08-19 15:57:35 +0800

    new:增加depth的测试

--- my_modules/depth_predict.py
+++ my_modules/depth_predict.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 import cv2
 import os
 
-model = YOLO(r"F:\Gitbase\ultralytics\pretrain_model\yolo26n-depth.pt", task="depth")
-img_path = r'F:\Gitbase\ultralytics\depth4_test.jpg'
+model = YOLO(r"/pretrain_model/yolo26n-depth.pt", task="depth")
+img_path = r'/depth4_test.jpg'
 if os.path.exists(img_path):
     img = cv2.imread(img_path)
 
